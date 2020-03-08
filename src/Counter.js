50e7bf9efaaea65edc44e7b72d82138e32b93fb0
SebastianGarces <gsgarces1@gmail.com>
2020-03-08 16:37:19 -0400

    completed refactoring class component section

--- src/Counter.js
+++ src/Counter.js
@@ -1,24 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 //Helper Function:
 //**DO NOT USE IN PRODUCTION
-const getStateFromLocalStorage = () => {
-	const storage = localStorage.getItem('counterStorage');
-	console.log(storage);
-
-	if (storage) return JSON.parse(storage).count;
-	return 0;
-};
-
-const storeStateInLocalStorage = count => {
-	localStorage.setItem('counterStorage', JSON.stringify({ count }));
-	console.log(localStorage);
-};
-
 const useLocalStorage = (initialState, key) => {
 	const get = () => {
 		const storage = localStorage.getItem(key);
-		console.log(localStorage, storage);
 		if (storage) return JSON.parse(storage).value;
 		return initialState;
 	};
@@ -27,28 +13,36 @@ const useLocalStorage = (initialState, key) => {
 
 	useEffect(() => {
 		localStorage.setItem(key, JSON.stringify({ value }));
-	}, value);
+	}, [value]);
 
 	return [value, setValue];
 };
 
-const Counter = ({ max, step }) => {
+const Counter = () => {
 	const [count, setCount] = useLocalStorage(0, 'count');
 
-	const increment = () => setCount(c => (c >= max ? c : c + step));
+	const increment = () => setCount(c => c + 1);
 	const decrement = () => setCount(count - 1);
 	const reset = () => setCount(0);
 
-	useEffect(() => {
-		document.title = `Counter: ${count}`;
-	}, [count]);
+	const countRef = useRef(null);
+	let message = '';
+	if (countRef.current < count) message = 'Higher';
+	if (countRef.current > count) message = 'Lower';
 
 	useEffect(() => {
-		storeStateInLocalStorage(count);
+		const id = setInterval(() => {
+			console.log(`Count: ${count}`);
+		}, 3000);
+
+		return () => {
+			clearInterval(id);
+		};
 	}, [count]);
 
 	return (
 		<div className="Counter">
+			<p>{message}</p>
 			<p className="count">{count}</p>
 			<section className="controls">
 				<button onClick={increment}>Increment</button>
