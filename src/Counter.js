eaa019b2aa7b2535989611f3e3d298207c362b91
SebastianGarces <gsgarces1@gmail.com>
2020-03-07 23:25:58 -0500

    keeping state with .bind()

--- src/Counter.js
+++ src/Counter.js
@@ -1,18 +1,50 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-  render() {
-    return (
-      <div className="Counter">
-        <p className="count">0</p>
-        <section className="controls">
-          <button>Increment</button>
-          <button>Decrement</button>
-          <button>Reset</button>
-        </section>
-      </div>
-    );
-  }
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			count: 0,
+		};
+
+		this.increment = this.increment.bind(this);
+		this.decrement = this.decrement.bind(this);
+		this.reset = this.reset.bind(this);
+	}
+
+	increment() {
+		this.setState(state => {
+			return { count: state.count + 1 };
+		});
+	}
+
+	decrement() {
+		this.setState(state => {
+			return { count: state.count - 1 };
+		});
+	}
+
+	reset() {
+		this.setState(state => {
+			return { count: 0 };
+		});
+	}
+
+	render() {
+		const { count } = this.state;
+
+		return (
+			<div className="Counter">
+				<p className="count">{count}</p>
+				<section className="controls">
+					<button onClick={this.increment}>Increment</button>
+					<button onClick={this.decrement}>Decrement</button>
+					<button onClick={this.reset}>Reset</button>
+				</section>
+			</div>
+		);
+	}
 }
 
 export default Counter;
